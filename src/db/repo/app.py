9973b59724a2292bda49979f26de772aed1195bb
NickoShmFree <nikolajsm05@gmail.com>
2025-12-27 23:33:38 +0300

    add repo ReadingList

--- src/db/repo/app.py
+++ src/db/repo/app.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 
 from .sa import SARepository
-from db.models import UserDB
+from db.models import UserDB, ReadingListDB, TagDB, ReadingListTagDB
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -10,3 +10,18 @@ if TYPE_CHECKING:
 class UserRepo(SARepository[UserDB]):
     def __init__(self, session: "AsyncSession"):
         super().__init__(session=session, model_type=UserDB)
+
+
+class ReadingListRepo(SARepository[ReadingListDB]):
+    def __init__(self, session: "AsyncSession"):
+        super().__init__(session=session, model_type=ReadingListDB)
+
+
+class TagRepo(SARepository[TagDB]):
+    def __init__(self, session: "AsyncSession"):
+        super().__init__(session=session, model_type=TagDB)
+
+
+class ReadingListTagRepo(SARepository[ReadingListTagDB]):
+    def __init__(self, session: "AsyncSession"):
+        super().__init__(session=session, model_type=ReadingListTagDB)
