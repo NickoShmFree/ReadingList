2eacccc485044206e294099642b97c8cab5de152
NickoShmFree <nikolajsm05@gmail.com>
2025-12-27 19:40:11 +0300

    add UserServiceDB

---
+++ src/db/services_db/__init__.py
@@ -0,0 +1,6 @@
+from .user import UserServiceDB
+
+
+__all__ = [
+    "UserServiceDB",
+]
---
+++ src/db/services_db/user.py
@@ -0,0 +1,38 @@
+from typing import TYPE_CHECKING, Union
+
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from db.repo.app import UserRepo
+from db.models import UserDB
+
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+    from schemas import UserCreateSchema
+
+
+class UserServiceDB:
+    """Сервис для работы с таблицей users (пользователи)"""
+
+    def __init__(self, session: "AsyncSession") -> None:
+        self.repo = UserRepo(session)
+
+    async def add(
+        self,
+        user: "UserCreateSchema",
+        hashed_password: str,
+    ) -> UserDB:
+        model = UserDB(
+            display_name=user.display_name,
+            email=user.email,
+            hashed_password=hashed_password,
+        )
+        await self.repo.add(model=model)
+        return model
+
+    async def get_by_email(self, email: str) -> UserDB | None:
+        return await self.repo.get(where=[UserDB.email == email])
+
+    async def get_by_id(self, user_id: int) -> UserDB | None:
+        return await self.repo.get(ident=user_id)
